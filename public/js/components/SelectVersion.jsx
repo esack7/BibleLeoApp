bde859f0cd7729848000664e62f6c141101eed91
Isaac Heist <isaacheist@gmail.com>
2018-02-04 15:27:45 -0800

    getting selectVersion to work, need to also get .env to import

--- public/js/components/SelectVersion.jsx
+++ public/js/components/SelectVersion.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
+import uuid from 'uuid/v4';
 
 class SelectVersion extends React.Component{
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      versions: [],
+      error: null,
+    };
   }
 
   componentDidMount() {
-    
+    console.log('API_KEY: ', process.env);
+    return fetch(`https://api.biblia.com/v1/bible/find?key=${process.env.API_KEY}`)
+      .then(res => res.json())
+      .then(jsonRes => {
+        this.setState({
+          versions: jsonRes.bibles
+        })
+      }, error => {
+        this.setState({
+          error
+        })
+      }
+    )
   }
 
   render() {
+    if(this.state.error) {
+      return(
+        <div>
+          <h1>An error has occurred: {this.state.error}</h1>
+        </div>
+      )
+    } 
     return(
       <div>
         <label htmlFor="version">
         Choose a Bible Version:
-        <select name="version" id="version" />
+        <select name="version" id="version">
+            {this.state.versions.map((idx) => <option key={uuid()} value={idx.bible}>{idx.title}</option>)}
+        </select>
         </label>
       </div>
     )

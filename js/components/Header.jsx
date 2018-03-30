0f85c9bfdaa502121e8b5c241ab16cc48cb6deab
esack7 <isaacheist@gmail.com>
2018-03-30 16:14:46 -0700

    Updates

--- js/components/Header.jsx
+++ js/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => (
-  <div>
-    <h1>LEO - Reading the Word of God daily</h1>
-  </div>
-);
+class Header extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>LEO - Reading the Word of God daily</h1>
+        <button
+          type="submit"
+          onClick={this.props.showSettings}
+        >Settings</button>
+      </div>
+    )
+  }
+}
+
+Header.propTypes = {
+  showSettings: PropTypes.func.isRequired
+}
 
 export default Header;

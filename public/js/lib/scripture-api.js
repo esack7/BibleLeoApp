ffd33859f8e437f710a5c18386d5a54484c76072
esack7 <isaacheist@gmail.com>
2018-04-08 08:08:45 -0700

    integrated server.js for making api calls, no longer a static site

--- public/js/lib/scripture-api.js
+++ public/js/lib/scripture-api.js
@@ -2,11 +2,11 @@ import superagent from 'superagent';
 
 const MakeCall = (version, verse) => new Promise((resolve, reject) =>
   resolve(
-    superagent(`https://api.biblia.com/v1/bible/content/${version}.txt.json`)
-      .query({ passage: verse })
-      .query({ key: process.env.API_KEY })
+    superagent(`/v1/bible/content`)
+      .query({ verse })
+      .query({ version })
       .then(res => res.body)
-      .then(jsonRes => jsonRes.text.split(' \r\n').map(idx => idx.trim()))
+      .then(jsonRes => jsonRes.split(' \r\n').map(idx => idx.trim()))
       .catch(err => console.error(err))
   )
     .catch(error => reject(error)));

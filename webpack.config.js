dca2297ea96d9b3a650cdbcd086bc45a856f551d
esack7 <isaacheist@gmail.com>
2018-04-08 08:17:32 -0700

    undo change in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ let plugins = [
 ];
 let entry = [
   'react-hot-loader/patch',
-  // 'webpack-dev-server/client?http://localhost:8080',
+  'webpack-dev-server/client?http://localhost:8080',
   'webpack/hot/only-dev-server',
   './public/js/ClientApp.jsx'
 ];
